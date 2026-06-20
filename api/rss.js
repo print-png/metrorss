de97fc78cc83efd2c6ebbeaffd7415920ada68d2
frombananas <kfilippov419@gmail.com>
2026-06-20 06:36:03 +0300

    Fix serverless: remove in-memory rate limiting, simplify for Vercel

--- api/rss.js
+++ api/rss.js
@@ -1,4 +1,3 @@
-const app = require('express')();
 const { kv } = require('@vercel/kv');
 
 function escapeXml(str) {
@@ -13,26 +12,24 @@ async function getPosts() {
     } catch (e) { return []; }
 }
 
-app.get('/', async (req, res) => {
+module.exports = async (req, res) => {
     const posts = await getPosts();
     res.set('Content-Type', 'application/rss+xml; charset=utf-8');
     const siteLink = "https://metrorss.vercel.app";
-    let rss = '<?xml version="1.0" encoding="UTF-8" ?>' + "\n" + '<rss version="2.0">' + "\n" + '<channel>' + "\n";
-    rss += '<title>MetroRSS Feed</title>' + "\n";
-    rss += '<link>' + siteLink + '</link>' + "\n";
-    rss += '<description>Глобальный RSS поток пользователей MetroRSS</description>' + "\n";
-    rss += '<language>ru</language>' + "\n";
+    let rss = '<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0">\n<channel>\n';
+    rss += '<title>MetroRSS Feed</title>\n';
+    rss += '<link>' + siteLink + '</link>\n';
+    rss += '<description>Глобальный RSS поток пользователей MetroRSS</description>\n';
+    rss += '<language>ru</language>\n';
     for (const post of posts) {
-        rss += '<item>' + "\n";
-        rss += '  <title>' + escapeXml(post.title) + '</title>' + "\n";
-        rss += '  <link>' + siteLink + '</link>' + "\n";
-        rss += '  <description>' + escapeXml(post.text) + '</description>' + "\n";
-        rss += '  <pubDate>' + post.date + '</pubDate>' + "\n";
-        rss += '  <guid isPermaLink="false">' + post.id + '</guid>' + "\n";
-        rss += '</item>' + "\n";
+        rss += '<item>\n';
+        rss += '  <title>' + escapeXml(post.title) + '</title>\n';
+        rss += '  <link>' + siteLink + '</link>\n';
+        rss += '  <description>' + escapeXml(post.text) + '</description>\n';
+        rss += '  <pubDate>' + post.date + '</pubDate>\n';
+        rss += '  <guid isPermaLink="false">' + post.id + '</guid>\n';
+        rss += '</item>\n';
     }
-    rss += '</channel>' + "\n" + '</rss>';
+    rss += '</channel>\n</rss>';
     res.send(rss);
-});
-
-module.exports = app;
+};
